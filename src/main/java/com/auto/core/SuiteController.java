27920db548c673eeb1849d151d70020057bfe572
wanghuanhuan <48585223+huanhuanyaya@users.noreply.github.com>
2020-04-12 02:17:31 +0800

    4.12

--- src/main/java/com/auto/core/SuiteController.java
+++ src/main/java/com/auto/core/SuiteController.java
@@ -8,7 +8,6 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
